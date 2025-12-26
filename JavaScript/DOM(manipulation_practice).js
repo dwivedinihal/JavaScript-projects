d8a6cfe10a1ab3186a5941f723bfba112e5ab904
dwivedinihal <dwivedinihal005@gmail.com>
2025-12-27 01:14:02 +0530

    Delete JavaScript/DOM(manipulation_practice).js

--- JavaScript/DOM(manipulation_practice).js
+++
@@ -1,7 +0,0 @@
-/*let newBtn = document.createElement("button");
-newBtn.innerText = "Click Me";
-newBtn.style.color = "Red";
-newBtn.style.backgroundColor = "Black";
-document.querySelector("body").prepend(newBtn);
-*/
-
