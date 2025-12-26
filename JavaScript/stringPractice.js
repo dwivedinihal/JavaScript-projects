17a8c80d98bfa19764d2b9cdc6b27998035f8ce4
dwivedinihal <dwivedinihal005@gmail.com>
2025-12-27 01:15:51 +0530

    Delete JavaScript/stringPractice.js

--- JavaScript/stringPractice.js
+++
@@ -1,4 +0,0 @@
-let fullname = prompt("Enter your full name without spaces : ");
-console.log("Your name : ", fullname);
-let username = "@" + fullname + fullname.length;
-console.log("Suggested username : " , username);
